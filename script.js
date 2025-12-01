147d29b305a08e2f401ef96d7063ef0cc985a3fc
SIMMY-KUMARI12 <simmykumari825@gmail.com>
2025-12-01 05:15:47 +0000

    storing the task in the local storage

--- script.js
+++ script.js
@@ -14,14 +14,28 @@ function addTask(){
         li.appendChild(span);
       }
       inputBox.value=" ";
+      saveData();    /* so to add the data to the localStorage when ever we add new task */
 }
 listcontainer.addEventListener("click",function(e){
     if(e.target.tagName === "LI")
     {
         e.target.classList.toggle("checked");
+        saveData();
     }
     else if(e.target.tagName === "SPAN")
     {
         e.target.parentElement.remove();
+        saveData();
     }
 },false);
+
+/*will store the data in the localStorage*/
+function saveData(){
+  localStorage.setItem("data", listcontainer.innerHTML);
+}
+
+/* now to display the locally stored data in the browser*/
+function showTask(){
+  listcontainer.innerHTML = localStorage.getItem("data");
+}
+showTask();
